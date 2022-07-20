6589fe5c28840a0696982b4d4d36d37a8bda238c
Andy Prior <andy@gocloud1001.com>
2022-07-20 13:34:08 -0400

    3.1.5b (js file updates) MAJOR : Wrong operator resulted in logging things in PROD you don't want logged!

--- FileCabinet/SuiteScripts/openSuite/netsuite-authnet/sac/AuthNet_lib.js
+++ FileCabinet/SuiteScripts/openSuite/netsuite-authnet/sac/AuthNet_lib.js
@@ -47,7 +47,7 @@
 
 define(["require", "exports", 'N/runtime', 'N/https', 'N/redirect', 'N/crypto', 'N/encode', 'N/log', 'N/record', 'N/search', 'N/format', 'N/error', 'N/config', 'N/cache', 'N/ui/message', 'moment', 'lodash', './anlib/AuthorizeNetCodes'],
     function (require, exports, runtime, https, redirect, crypto, encode, log, record, search, format, error, config, cache, message, moment, _, codes) {
-    exports.VERSION = '3.1.5a';
+    exports.VERSION = '3.1.5b';
 
     //all the fields that are custbody_authnet_ prefixed
     exports.TOKEN = ['cim_token'];
@@ -184,7 +184,7 @@ define(["require", "exports", 'N/runtime', 'N/https', 'N/redirect', 'N/crypto', 
 
     exports.homeSysLog = function(name, body)
     {
-        if(exports.getConfigFromCache().custrecord_an_break_pci.val || runtime.envType !== runtime.EnvType.SANDBOX)
+        if(exports.getConfigFromCache().custrecord_an_break_pci.val || runtime.envType === runtime.EnvType.SANDBOX)
         {
             log.debug('&#10071; ' + name, body);
         }
