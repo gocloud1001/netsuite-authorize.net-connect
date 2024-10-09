68744f7bb612f0e03f1ae8e8bc94204ad36b5d42
Andy Prior <andy@gocloud1001.com>
2024-10-09 14:00:06 -0400

    Commits of the HTML side of things that need to be updated

---
+++ FileCabinet/SuiteScripts/openSuite/netsuite-authnet/sac/click2pay/html/authnet_click2pay_onlinepayment_vaildation.js
@@ -0,0 +1,27 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+
+    'use strict'
+    //alert("I am here");
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            else
+            {
+                document.getElementById('submitbutton').disabled = true;
+                document.getElementById('submitbutton').style.opacity='0.5';
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    });
+
+})()
